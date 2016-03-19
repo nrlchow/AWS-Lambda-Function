bb60cec95ba83c88d705b103837bb6281723bae5
nrlchow <nrlchow@gmail.com>
2016-03-19 19:20:25 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+var faker = require('faker');
+
+exports.handler = function (event,context) {
+    
+    var inventory = [];
+    for (var i=;i<10;i++) {
+        var shoe = {};
+        var shoeType = getShoeType{};
+        shoe.name = getShoeName(shoeType);
+        shoe.color = getShoeColor();
+        shoe.description = getShoeDescription(shoeType);
+        shoe.size = getShoeSize();
+        shoe.price = getShoePrice();
+        inventory.push(shoe);
+                
+        }
+    context.succeed(inventory);
+}
+
+
+
+function getShoeName(shoeType) {
+    retun faker.company.catchPhraseNoun() + "" + faker.company.catchPhraseDescriptor() + 
+        "" + shoeType;
+    
+}
+
+function getShoeColor(){
+    return faker.commerce.color();
+}
+
+function getShoeDescription(shoeType) {
+    return "A(n)" + faker.commerce.productAdjective()+"," + faker.commerce.productAdjective() + 
+        " "+ shoeType + "made from the finest" + faker.commerce.productMaterial()+ "desined for the " + faker.company.bsBuzz() + "individual!"
+        
+    
+}
+
+function getShoeSize() {
+    return getNum(1,13);
+}
+
+
+function getshoePrice() {
+    return faker.commerce.price();
+}
+function getshoeType() {
+    
+    var shoeType = {
+        "running shoe",
+        "cross-training shoe",
+        "tennis shoe",
+        "basketball shoe",
+        "aerobic shoe",
+        "spinning shoe"
+    }
+    retun shoeType[getNum(0,5)];
+    
+}
+
+
+function getNum (min,max) {
+    return Math.floor(math.random()*(max-min+1)) + min;
+}
